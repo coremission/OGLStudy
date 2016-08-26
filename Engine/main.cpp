68a41655721fd84928ea94de4c01f85fb37f4c82
Sergey Kormishin <s.coremission@gmail.com>
2016-08-26 13:43:27 +0300

    engine project type is static library now

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,27 +1,27 @@
-#include "Application.h"
-#include <iostream>
-#include <memory>
-
-const char * TRIANGLE_NAME = "triangle1";
-
-int main(int argc, char **argv)
-{
-	auto sceneShared = std::make_shared<Models::Scene>();
-	
-	try {
-		Application::initialize(&argc, argv);
-		Application::setUpScene(sceneShared);
-		sceneShared->CreateTriangleModel(TRIANGLE_NAME);
-
-		Application::runMainLoop();
-	}
-	catch (std::exception exc) {
-		Application::exit();
-		std::cout << exc.what();
-	}
-
-	std::cout << std::endl << "Press any key to exit..." << std::endl;
-	std::cin.get();
-	
-	return 0;
-}
+//#include "Application.h"
+//#include <iostream>
+//#include <memory>
+//
+//const char * TRIANGLE_NAME = "triangle1";
+//
+//int main(int argc, char **argv)
+//{
+//	auto sceneShared = std::make_shared<Models::Scene>();
+//	
+//	try {
+//		Application::initialize(&argc, argv);
+//		Application::setUpScene(sceneShared);
+//		sceneShared->CreateTriangleModel(TRIANGLE_NAME);
+//
+//		Application::runMainLoop();
+//	}
+//	catch (std::exception exc) {
+//		Application::exit();
+//		std::cout << exc.what();
+//	}
+//
+//	std::cout << std::endl << "Press any key to exit..." << std::endl;
+//	std::cin.get();
+//	
+//	return 0;
+//}
