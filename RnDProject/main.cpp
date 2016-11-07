b7d2f6f827a8ad45ed76b25714502230c3c2ef23
Sergey Kormishin <sergey.kormishin@nival.com>
2016-11-07 11:00:34 +0300

    rendering arc refactoring

--- RnDProject/main.cpp
+++ RnDProject/main.cpp
@@ -1,10 +1,23 @@
 ﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+#include "rendering.hpp"
 
 using namespace std;
 
+void _do();
+
 int main(int argc, char** argv)
 {
+	_do();
+
 	cout << endl << "Press any key to exit..." << endl;
 	cin.get();
 	return 0;
+}
+
+void _do() {
+	unique_ptr<IRenderer> renderer = std::make_unique<CubeRenderer>();
+	renderer->render();
 }
---
+++ RnDProject/rendering.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+// Basic interface to Use in GameObject
+class IRenderer {
+public:
+	virtual void render() {};
+	virtual ~IRenderer() = default;
+};
+
+template<typename VertexData>
+struct Mesh {
+	std::vector<VertexData> data;
+};
+
+template<typename DerivedRenderer, typename Traits>
+class Renderer : public IRenderer {
+	Mesh<typename Traits::VertexDataType> mesh;
+	typename Traits::UniformDataType uniformData;
+
+	constexpr DerivedRenderer* derived() { return static_cast<DerivedRenderer*>(this); }
+public:
+	virtual void render() override {
+		// 1. bind mesh to context (bind VAO)
+
+		// 2. use shader program
+
+		// 3. set up uniform parameters
+
+		// 4. draw (glDrawArrays, glDrawElements if mesh is indexed)
+	};
+	virtual ~Renderer() = default;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SPECIFIC RENDERER IMPLEMENTATION //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct CubeMaterialTraits {
+	class PerVertexData {
+		int position;
+		int normal;
+		int color;
+		int uv;
+	};
+
+	struct UniformData {
+		int diffuseTexture;
+		int specularTexture;
+		int Time;
+		int ModelingMatrix;
+		int MVPMatrix;
+		int IT_MVPMatrix;
+		// ... etc
+	};
+
+	typedef UniformData	UniformDataType;
+	typedef PerVertexData VertexDataType;
+};
+
+class CubeRenderer : public Renderer<CubeRenderer, CubeMaterialTraits> {
+	friend class Renderer<CubeRenderer, CubeMaterialTraits>;
+public:
+	virtual void render() override { std::cout << "render cube perfectly"; };
+	virtual ~CubeRenderer() override { std::cout << std::endl << "~CubeRenderer" << std::endl; };
+};
